8f0c56549eadf42c7613ac5ef06c8eef6b208345
Muhannad <tech.muhannadali@gmail.com>
2025-11-23 18:42:49 +0100

    Add MindTilt light/dark color palettes

--- app/src/main/java/com/griffith/mindtilt/ui/theme/Color.kt
+++ app/src/main/java/com/griffith/mindtilt/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package com.griffith.mindtilt.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light theme
+val md_theme_light_primary = Color(0xFF6C63FF)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xFF00BFA6)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFF6F6F6)
+val md_theme_light_onBackground = Color(0xFF1B1B1F)
+val md_theme_light_surface = Color(0xFFFFFFFF)
+val md_theme_light_onSurface = Color(0xFF1B1B1F)
+val md_theme_light_error = Color(0xFFB00020)
+val md_theme_light_onError = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark theme
+val md_theme_dark_primary = Color(0xFF9E9DFF)
+val md_theme_dark_onPrimary = Color(0xFF1B1B1F)
+val md_theme_dark_secondary = Color(0xFF4DD0C8)
+val md_theme_dark_onSecondary = Color(0xFF1B1B1F)
+val md_theme_dark_background = Color(0xFF121212)
+val md_theme_dark_onBackground = Color(0xFFE1E1E1)
+val md_theme_dark_surface = Color(0xFF1E1E1E)
+val md_theme_dark_onSurface = Color(0xFFE1E1E1)
+val md_theme_dark_error = Color(0xFFCF6679)
+val md_theme_dark_onError = Color(0xFF1B1B1F)
