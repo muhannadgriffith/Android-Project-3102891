d732a3472bc8799dc176d6aa56ffb94ee0a51ef3
Muhannad <tech.muhannadali@gmail.com>
2025-11-30 14:49:07 +0000

    Add Gradient colors

--- app/src/main/java/com/griffith/mindtilt/ui/theme/Color.kt
+++ app/src/main/java/com/griffith/mindtilt/ui/theme/Color.kt
@@ -14,6 +14,12 @@ val md_theme_light_onSurface = Color(0xFF1B1B1F)
 val md_theme_light_error = Color(0xFFB00020)
 val md_theme_light_onError = Color(0xFFFFFFFF)
 
+// Gradient colors - Light theme
+val gradient_light_start = Color(0xFF8A7CFF)
+val gradient_light_middle = Color(0xFFB49BFF)
+val gradient_light_end = Color(0xFFE1CFFF)
+
+
 // Dark theme
 val md_theme_dark_primary = Color(0xFF9E9DFF)
 val md_theme_dark_onPrimary = Color(0xFF1B1B1F)
@@ -25,3 +31,9 @@ val md_theme_dark_surface = Color(0xFF1E1E1E)
 val md_theme_dark_onSurface = Color(0xFFE1E1E1)
 val md_theme_dark_error = Color(0xFFCF6679)
 val md_theme_dark_onError = Color(0xFF1B1B1F)
+
+// Gradient colors - Dark theme
+val gradient_dark_start = Color(0xFF3B3270)
+val gradient_dark_middle = Color(0xFF5A4FA8)
+val gradient_dark_end = Color(0xFF8A7CFF)
+
