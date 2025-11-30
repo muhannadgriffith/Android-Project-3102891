b05a2f4387622f2336d51a59acc33a7fc392f258
Muhannad <tech.muhannadali@gmail.com>
2025-11-30 14:48:03 +0000

    Add basic HistoryScreen

--- app/src/main/java/com/griffith/mindtilt/ui/history/HistoryScreen.kt
+++ app/src/main/java/com/griffith/mindtilt/ui/history/HistoryScreen.kt
@@ -4,29 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.griffith.mindtilt.ui.theme.MindTiltTheme
 
 
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MindTiltTheme {
-        Greeting("Android")
+fun HistoryScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("History", fontSize = 22.sp)
     }
 }
