44878ef8659690c2f1289e1a6cc7f368fe01a248
Muhannad <tech.muhannadali@gmail.com>
2025-11-30 14:52:25 +0000

    Redesign WelcomeScreen

--- app/src/main/java/com/griffith/mindtilt/ui/onboarding/WelcomeScreen.kt
+++ app/src/main/java/com/griffith/mindtilt/ui/onboarding/WelcomeScreen.kt
@@ -3,16 +3,8 @@
 
 package com.griffith.mindtilt.ui.onboarding
 
-import android.R.attr.singleLine
-import android.R.attr.top
-import android.content.Intent
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -34,8 +26,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -50,11 +40,9 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.griffith.mindtilt.ui.theme.LocalGradients
-import com.griffith.mindtilt.ui.theme.MindTiltTheme
 
 @Composable
 fun WelcomeScreen(onNextClick: (String) -> Unit) {
