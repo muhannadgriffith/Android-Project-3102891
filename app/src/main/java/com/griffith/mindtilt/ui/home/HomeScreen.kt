10e345a853ad125be3d003eb944de24b29b8d308
Muhannad <tech.muhannadali@gmail.com>
2025-12-13 18:13:17 +0000

    Use rememberSaveable instead of remember

--- app/src/main/java/com/griffith/mindtilt/ui/home/HomeScreen.kt
+++ app/src/main/java/com/griffith/mindtilt/ui/home/HomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -47,7 +48,7 @@ fun HomeScreen(
     val gradientColors = LocalGradients.current.primary
 
     // State to store the currently selected mood
-    var selectedMood by remember { mutableStateOf<String?>(null) }
+    var selectedMood by rememberSaveable { mutableStateOf<String?>(null) }
 
     // Scaffold to handle system insets
     Scaffold { innerPadding ->
@@ -128,7 +129,7 @@ fun HomeScreen(
 
                 Spacer(modifier = Modifier.height(32.dp))
 
-                /// Button that navigates into the meditation session screen with selected mood
+                // Button that navigates into the meditation session screen with selected mood
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
